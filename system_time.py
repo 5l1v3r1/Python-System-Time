4706c9674df679bbd443db5a6a09ebed9cebc9ef
İSMAİL TAŞDELEN <pentestdatabase@gmail.com>
2016-04-17 17:22:14 +0300

    Added files via upload

---
+++ system_time.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#########################################################
+#              SYSTEM TIME - GH0ST S0FTWARE             #
+######################################################### 
+#                       CONTACT                         #
+#########################################################
+#              DEVELOPER : İSMAİL TAŞDELEN              #                       
+#        Mail Address : pentestdatabase@gmail.com       #
+# LINKEDIN : https://www.linkedin.com/in/ismailtasdelen #
+#           Whatsapp : + 90 534 295 94 31               #
+#########################################################
+
+import datetime
+
+zaman = datetime.datetime.now()
+datetime.datetime.ctime(zaman)
+print '******************************'
+print '*',zaman,'*'
+print '******************************'
+
